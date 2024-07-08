e487e891aec3c81f4f245c404d1507aba652716b
Hyungsuk Kang <hskang9@gmail.com>
2024-07-08 11:56:17 +0200

    upate luminaERC20 to Augment

---
+++ contracts/src/SourceAugment.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract LuminaTeleportERC20 is
+    ERC20,
+    ERC20Burnable,
+    ERC20Pausable,
+    AccessControl
+{
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    address public intentSender;
+
+    constructor(string memory name, string memory symbol, address intentSender) ERC20(name, symbol) {
+        // Grant the contract deployer the default admin role: they can grant and revoke any roles
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        // Grant the minter and pauser roles to the deployer
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        intentSender=intentSender;
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function teleport(address to, uint256 amount) external {
+        _burn(msg.sender, amount);
+        // build abi function call
+        bytes selector = bytes4(keccak256(bytes("mint(address,uint256)")));
+        IIntentSender(intentSender).sendIntent(to, abi.encodeWithSelector(selector, to, value));
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override(ERC20, ERC20Pausable) whenNotPaused {
+        super._update(from, to, value);
+    }
+}
